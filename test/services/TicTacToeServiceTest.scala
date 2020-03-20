ca17eff0d0ecb32d2e0885d84e1792fcb31ef5dc
Muyinda ROgers <moverr@gmail.com>
2020-03-20 19:32:42 +0300

    :Init Game  Test

--- test/services/TicTacToeServiceTest.scala
+++ test/services/TicTacToeServiceTest.scala
@@ -2,6 +2,7 @@ package services
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -9,11 +10,13 @@ import play.api.test.Helpers._
 
 class TicTacToeServiceTest extends PlaySpec  with GuiceOneAppPerTest with Injecting {
 
- "Testing the InitGame" should  {
+ "Testing the InitGame" in   {
    var board:String = "++++++++++";
-    var service:TicTacToeService =   new TicTacToeService(board)
+   var service:TicTacToeService =   new TicTacToeService(board)
+   var response = service.initGame()
+   var expectedResult = Json.toJson(board)
+   response mustEqual expectedResult
 
-    Conte
 
  }
 
