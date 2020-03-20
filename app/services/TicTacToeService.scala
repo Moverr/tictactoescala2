c738470b69a13318de0c968576155e33091a53ea
Muyinda ROgers <moverr@gmail.com>
2020-03-20 18:57:16 +0300

    Init app :

--- app/services/TicTacToeService.scala
+++ app/services/TicTacToeService.scala
@@ -2,14 +2,19 @@ package services
 
 import com.fasterxml.jackson.annotation.JsonValue
 import javax.inject.Singleton
+import play.api.libs.json.{JsValue, Json}
+
 
 trait ITicTacToeService{
-  def initGame(boardString: String): JsonValue
+  def initGame(): JsValue
 }
 //todo;
 @Singleton
 class TicTacToeService(val board: String) extends ITicTacToeService {
-  override def initGame(boardString: String): JsonValue = {
-    return null
+  val boardString :String = board;
+
+    @Override def initGame(): JsValue = {
+    val  result = Json.toJson(this.boardString)
+    return result;
   }
 }
